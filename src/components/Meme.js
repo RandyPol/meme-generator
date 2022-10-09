8a7346daa94a85ea0f626b1df09a6e2df65d099a
Ran Pol <rnd809@gmail.com>
2022-10-09 10:42:09 -0400

    Refactor the meme component

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,27 +1,41 @@
 import React from 'react'
+import memesData from '../data/memesData'
 
 const Meme = () => {
+  const [memeImage, setMemeImage] = React.useState('')
+  const memesArray = memesData.data.memes
+
+  const getNewImage = () => {
+    const numOfMemes = () =>
+      memesArray[Math.floor(Math.random() * memesArray.length)].url
+
+    setMemeImage(numOfMemes)
+  }
+
+  
   return (
     <main>
-      <form className="meme-form">
-          <input
-            className="meme-input"
-            type="text"
-            name="top-text"
-            placeholder="Top Text"
-            required
-          />
-          <input
-            className="meme-input"
-            type="text"
-            name="bottom-text"
-            placeholder="Bottom Text"
-            required
-          />
-        <button className="meme-button" type="submit">
+      <div className="meme-form">
+        <input
+          className="meme-input"
+          type="text"
+          name="top-text"
+          placeholder="Top Text"
+          required
+        />
+        <input
+          className="meme-input"
+          type="text"
+          name="bottom-text"
+          placeholder="Bottom Text"
+          required
+        />
+        <button className="meme-button" type="submit" onClick={getNewImage}>
           Get a new meme image 🖼
         </button>
-      </form>
+      </div>
+
+      <img src={memeImage} alt="newImage" className="meme-img" />
     </main>
   )
 }
