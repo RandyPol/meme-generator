05f726c1c8f67696a8ac58414cab97c58beccdd8
Ran Pol <rnd809@gmail.com>
2022-10-10 17:45:42 -0400

    Set up the text inputs to save to the 'topText' and 'bottomText' state variable. Replace the hard-coded text on the image with the text being saved to state.

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -4,42 +4,50 @@ import memesData from '../data/memesData'
 const Meme = () => {
   const [allMemeImages, setAllMemeImages] = React.useState(memesData.data.memes)
   const [meme, setMeme] = React.useState({
-    topText: 'Top text',
-    bottomText: 'Bottom text',
+    topText: '',
+    bottomText: '',
     randomImage: 'https://i.imgflip.com/30b1gx.jpg',
   })
 
-  const getMeme = () => {
+  const getMeme = (e) => {
     const numOfMemes =
       allMemeImages[Math.floor(Math.random() * allMemeImages.length)].url
+    const { name, value, type } = e.target
 
     setMeme((prev) => ({
       ...prev,
-      randomImage: numOfMemes,
+      randomImage: type === 'submit' ? numOfMemes : prev.randomImage,
+      [name]: value,
     }))
+  }
+console.log(meme)
+  const submitHandle = (e) => {
+    e.preventDefault()
   }
 
   return (
     <main>
-      <div className="meme-form">
+      <form onSubmit={submitHandle} className="meme-form">
         <input
           className="meme-input"
           type="text"
-          name="top-text"
+          name="topText"
           placeholder="Top Text"
-          required
+          value={meme.topText}
+          onChange={getMeme}
         />
         <input
           className="meme-input"
           type="text"
-          name="bottom-text"
+          name="bottomText"
           placeholder="Bottom Text"
-          required
+          value={meme.bottomText}
+          onChange={getMeme}
         />
         <button className="meme-button" type="submit" onClick={getMeme}>
           Get a new meme image 🖼
         </button>
-      </div>
+      </form>
       <p>{meme.topText}</p>
       <img src={meme.randomImage} alt="newImage" className="meme-img" />
       <p>{meme.bottomText}</p>
